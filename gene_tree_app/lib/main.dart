fc73b11cc82e006c641f1be9bb3554ea1f93cda0
aitc <aitc.os@mbbank.com.vn>
2024-10-18 14:58:15 +0700

    + config firebase

--- gene_tree_app/lib/main.dart
+++ gene_tree_app/lib/main.dart
@@ -1,6 +1,27 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:gene_tree_app/firebase_options.dart';
+// import 'firebase_options.dart';
 
-void main() {
+late final FirebaseApp app;
+late final FirebaseAuth auth;
+
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  // We're using the manual installation on non-web platforms since Google sign in plugin doesn't yet support Dart initialization.
+  // See related issue: https://github.com/flutter/flutter/issues/96391
+
+  // We store the app and auth to make testing with a named instance easier.
+  try {
+    app = await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+    auth = FirebaseAuth.instanceFor(app: app);
+  } catch (e) {
+    print("Error: ${e}");
+  }
+
   runApp(const MyApp());
 }
 
