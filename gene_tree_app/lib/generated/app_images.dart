1d29517eed4c9001cbbd5ddbe70d2c155a84a384
aitc <aitc.os@mbbank.com.vn>
2024-10-26 20:44:41 +0700

    * config

--- gene_tree_app/lib/generated/app_images.dart
+++ gene_tree_app/lib/generated/app_images.dart
@@ -1,5 +1,9 @@
 // This file is auto-generated. Do not modify it manually.
 
 class AppImages {
+  static const String onboarding_4 = 'assets/images/onboarding_4.png';
+  static const String onboarding_1 = 'assets/images/onboarding_1.png';
+  static const String onboarding_3 = 'assets/images/onboarding_3.png';
+  static const String onboarding_2 = 'assets/images/onboarding_2.png';
   static const String avatar = 'assets/images/avatar.jpg';
 }
